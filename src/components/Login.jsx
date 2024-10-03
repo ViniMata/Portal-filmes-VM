0db755034bbc4e109ce2c09894d9a30cefd67e0d
ViniMata <vinicius.matareli2005@gmail.com>
2024-10-02 22:30:52 -0300

    att

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ export default function Login({ isLogged, handleLogin }) {
         <>
             <div className="flex gap-4 items-center">
                 {isLogged && <p>Olá, Usuario.</p>}
-                <button onClick={handleLogin} className={`bg-${isLogged ? "white" : "bg-blue-500"} text-purple-800 px-4 py-1 rounded`} >
+                <button onClick={handleLogin} className={`bg-${isLogged ? "white" : "bg-blue-500"}${isLogged ? "active" : "undefined"} text-purple-800 px-4 py-1 rounded`} >
                     {isLogged ? "Logout" : "Login"}
                 </button>
             </div>
